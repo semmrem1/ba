2c373b267b27640000bb4573ba67364341968e96
semmrem1 <remo.semm@gmail.com>
2020-03-28 09:35:23 +0100

    build for heroku

---
+++ dist/precache-manifest.367eb884fc7c4c40618427c90794c0c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8af2a0ad0f61d2c80806",
+    "url": "/css/app.a3898e2a.css"
+  },
+  {
+    "revision": "86d3b0f7ec62f43c2f71",
+    "url": "/css/chunk-vendors.21add209.css"
+  },
+  {
+    "revision": "0c7c32f21606095293fdc7760737e549",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8af2a0ad0f61d2c80806",
+    "url": "/js/app.67effce4.js"
+  },
+  {
+    "revision": "86d3b0f7ec62f43c2f71",
+    "url": "/js/chunk-vendors.39e86205.js"
+  },
+  {
+    "revision": "18c0fc1ad52c6836e3523786049a24af",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
